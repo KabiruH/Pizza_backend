3308a5e5b300650aefea9107e2df8776c2ee6dfc
King <kelkab46@gmail.com>
2023-03-21 21:14:38 +0300

    completed assignment

--- app/serializers/restaurant_serializer.rb
+++ app/serializers/restaurant_serializer.rb
@@ -1,5 +1,9 @@
 class RestaurantSerializer < ActiveModel::Serializer
   attributes :id, :name, :address
 
-  has_many :pizza1s
+  def include_pizza1s?
+    @instance_options[:include_pizza1s]
+  end
+
+  has_many :pizza1s, if: :include_pizza1s?
 end
